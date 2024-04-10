8ce8095125a53648f891fb0e59aafc24d852eeac
omartins-zs <gabrielmatheus209@gmail.com>
2024-04-10 07:24:23 -0300

    Add Redirect na Pagina Autenticada

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,13 @@
 import { getServerSession } from "next-auth"
+import { redirect } from "next/navigation"
 
 export default async function Page() {
     const session = await getServerSession()
+
+    if (!session) {
+        redirect("/")
+    }
+
     return (
         <div>
             <div>Ola, {session?.user?.name}</div>
