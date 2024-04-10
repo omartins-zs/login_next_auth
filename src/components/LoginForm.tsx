97e4f90d4524fea508d9b7f0e3eb6a92c7d591cb
omartins-zs <gabrielmatheus209@gmail.com>
2024-04-10 07:20:46 -0300

    Criando Pagina de Login

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,21 +1,34 @@
 "use client";
 
+import { signIn } from "next-auth/react"
+
 export default function LoginForm() {
+    async function login(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+        const formData = new FormData(e.currentTarget)
+
+        console.log(e)
+
+        const data = {
+            email: formData.get("email"),
+            senha: formData.get("password")
+        };
+
+        signIn("credentials", {
+            ...data, callbackUrl: "./dashboard"
+        })
+
+        console.log(data)
+    }
     return (
-        <main>
+        <form onSubmit={login} className="bg-white p-12 rounded-lg w-96 max-w-full flex justify-center items-center flex-col gap-3">
 
-            <div className="h-screen flex justify-center items-center bg-slate-600 px-5">
+            <h2 className="font-blod text-xl mb-4">Faça seu Login</h2>
 
-                <form className="bg-white p-12 rounded-lg w-96 max-w-full flex justify-center items-center flex-col gap-3">
+            <input type="text" placeholder="Email" name="email" className="input input-primary w-full" />
+            <input type="password" placeholder="Senha" name="password" className="input input-primary w-full" />
 
-                    <h2 className="font-blod text-xl mb-4">Faça seu Login</h2>
-
-                    <input type="text" placeholder="Email" className="input input-primary w-full" />
-                    <input type="password" placeholder="Senha" className="input input-primary w-full" />
-
-                    <button className="btn btn-primary w-full">Login</button>
-                </form>
-            </div>
-        </main>
+            <button className="btn btn-primary w-full">Login</button>
+        </form>
     );
 }
