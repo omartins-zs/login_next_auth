315da700bc1ea963c1250538528cc173070c4408
omartins-zs <gabrielmatheus209@gmail.com>
2024-04-11 11:26:02 -0300

    Importando o Login Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
 
 
-import Image from "next/image";
+import { LoginBtn } from "./LoginBtn";
 
 export default function Home() {
   return (
     <main className="flex justify-center items-center h-screen">
-
-      <button className="flex gap-2 items-center border border-neutral-900 p-2 rounded-md">
-        <Image src="/g-icon.png" alt="" width={32} height={32} />
-        Login com Google
-      </button>
-
+      <LoginBtn />
     </main>
   );
 }
-
