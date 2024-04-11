22c467e4ba6c4f9e8c431ec0037c8eb47edb16c2
omartins-zs <gabrielmatheus209@gmail.com>
2024-04-11 02:07:05 -0300

    Fazendo Teste e Implementando Login Form

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -3,22 +3,31 @@
 import { signIn } from "next-auth/react"
 
 export default function LoginForm() {
-    async function login(e: React.FormEvent<HTMLFormElement>) {
-        e.preventDefault()
-        const formData = new FormData(e.currentTarget)
 
-        console.log(e)
+    async function login(e: React.FormEvent<HTMLFormElement>) {
+
+        e.preventDefault()
+
+        const formData = new FormData(e.currentTarget)
 
         const data = {
             email: formData.get("email"),
-            senha: formData.get("password")
+            password: formData.get("password"),
         };
 
-        signIn("credentials", {
-            ...data, callbackUrl: "./dashboard"
-        })
+        console.log("Dados do formulário:", data)
 
-        console.log(data)
+        try {
+
+            const res = await signIn("credentials", {
+                ...data, callbackUrl: "/dashboard"
+            })
+
+            console.log("Resposta", res)
+
+        } catch (error) {
+            console.log("error", error)
+        }
     }
     return (
         <form onSubmit={login} className="bg-white p-12 rounded-lg w-96 max-w-full flex justify-center items-center flex-col gap-3">
