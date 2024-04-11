a3941a9f6583d787dc00376aec80d2fc521e222d
omartins-zs <gabrielmatheus209@gmail.com>
2024-04-11 11:27:07 -0300

    Criando View Logado Dashboard

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,25 @@
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation"
+
+export default async function Page() {
+
+    const session = await getServerSession();
+
+    if (!session) {
+        redirect("/");
+    }
+
+    return (
+        <div className="container mx-auto">
+            <div>
+                <div className="text-cyan-500">Olá, {session.user?.name}</div>
+                <br />
+                <div className="text-orange-400">Email: {session.user?.email}</div>
+                <br />
+                <div className="text-lime-700">Image url: {session.user?.image}</div>
+            </div>
+        </div>
+    );
+}
+
+
