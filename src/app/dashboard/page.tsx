fc814fd502c1b9ec46daee3a7674e43dec900157
omartins-zs <gabrielmatheus209@gmail.com>
2024-04-12 10:59:14 -0300

    Criando Page Dashboard User Logado

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,26 @@
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation"
+
+export default async function Page() {
+
+    const session = await getServerSession();
+
+    console.log("Session", session)
+
+
+    if (!session) {
+        redirect("/");
+    }
+
+    return (
+        <div className="container mx-auto">
+            <div>
+                <div className="text-cyan-500">Olá, {session.user?.name}</div>
+                <br />
+                <div className="text-lime-700">Image URL: {session.user?.image}</div>
+                <br />
+            </div>
+        </div>
+    );
+}
+
