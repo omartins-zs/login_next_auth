bf31a7e3fbdfbd46f41d350051fe194c8cd84edd
omartins-zs <gabrielmatheus209@gmail.com>
2024-04-12 10:59:44 -0300

    Ajuste no GithubProviders e CallbackURL da Route

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,21 @@
-import NextAuth from "next-auth"
+import { NextApiHandler } from 'next';
+import GitHubProvider from 'next-auth/providers/github';
+import NextAuth, { NextAuthOptions } from 'next-auth';
 
 const handler = NextAuth({
-  ...
-})
+    providers: [
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID!,
+            clientSecret: process.env.GITHUB_SECRET!,
+        }),
+
+    ],
+    callbacks: {
+        async redirect({ url, baseUrl }) {
+            return Promise.resolve('http://localhost:3000/dashboard')
+        },
+    },
+    debug: true,
+});
 
 export { handler as GET, handler as POST }
